a73f03b16481b532bc0241dbac3a3a8e21424f8c
Jae Jeong <jeongjj29@gmail.com>
2024-10-09 16:12:34 -0400

    feat: add routes to back office and children

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,6 +2,11 @@ import React from "react";
 import App from "./components/App";
 import Home from "./pages/HomePage/Home";
 import ErrorPage from "./pages/ErrorPage";
+import BackOffice from "./pages/BackOfficePage/BackOffice";
+import MenuManagement from "./pages/MenuManagementPage/MenuManagement";
+import EmployeeManagement from "./pages/EmployeeManagementPage/EmployeeManagement";
+import TableManagement from "./pages/TableManagementPage/TableManagement";
+import SalesReports from "./pages/SalesReportsPage/SalesReports";
 
 const routes = [
   {
@@ -16,6 +21,28 @@ const routes = [
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/backoffice",
+        element: <BackOffice />,
+        children: [
+          {
+            path: "/menu",
+            element: <MenuManagement />,
+          },
+          {
+            path: "/employees",
+            element: <EmployeeManagement />,
+          },
+          {
+            path: "/tables",
+            element: <TableManagement />,
+          },
+          {
+            path: "/reports",
+            element: <SalesReports />,
+          },
+        ],
       },
     ],
   },

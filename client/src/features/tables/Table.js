a69c40a6893843852ca5e13e06008c269efc632b
Jae Jeong <jeongjj29@gmail.com>
2024-10-12 17:38:01 -0400

    feat: create basic table layout

---
+++ client/src/features/tables/Table.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Table({ isTable, table }) {
+  return <div className="w-12 h-12 bg-black"></div>;
+}
+
+export default Table;
--- client/src/features/tables/tableLayout.js
+++ client/src/features/tables/tableLayout.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchTables } from "./tablesSlice";
+import Table from "./Table";
 
 function TablesLayout() {
   const dispatch = useDispatch();
@@ -12,27 +13,60 @@ function TablesLayout() {
     dispatch(fetchTables());
   }, [dispatch]);
 
-  console.log(tables);
+  // console.log(tables);
+  //   {
+  //     "capacity": 6,
+  //     "id": 1,
+  //     "location_x": null,
+  //     "location_y": null,
+  //     "number": 1,
+  //     "orders": []
+  // }
 
-  const createEmptyTableLayout = (rows, cols) => {
+  const rows = 6;
+  const cols = 12;
+
+  const createEmptyTableLayout = (r, c) => {
     const tableLayout = [];
 
-    for (let i = 0; i < rows; i++) {
+    for (let i = 0; i < r; i++) {
       const row = [];
-      for (let j = 0; j < cols; j++) {
-        row.push({ id: `${i}-${j}`, status: "available" });
+      for (let j = 0; j < c; j++) {
+        row.push({ isTable: false });
       }
       tableLayout.push(row);
     }
     return tableLayout;
   };
+  const emptyTableLayout = createEmptyTableLayout(rows, cols);
 
-  console.log(createEmptyTableLayout(12, 6));
+  const loadTables = () => {
+    tables.forEach((table) => {
+      if (table["location_x"] === null || table["location_y"] === null) {
+        return;
+      }
+      const row = table["location_x"];
+      const col = table["location_y"];
+      emptyTableLayout[row][col] = { isTable: true, ...table };
+    });
+  };
+
+  loadTables();
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error}</p>;
 
-  return <div className="grid grid-cols-12 grid-rows-6 gap-4"></div>;
+  return (
+    <div>
+      {emptyTableLayout.map((row, i) => (
+        <div key={i} className="flex flex-row m-2 gap-2">
+          {row.map((col, j) => (
+            <Table key={j} isTable={col.isTable} table={col} />
+          ))}
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default TablesLayout;

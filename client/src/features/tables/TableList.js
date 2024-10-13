c777b8e47061f713cb6a6e4b259f7a66beb293f6
Jae Jeong <jeongjj29@gmail.com>
2024-10-13 18:30:06 -0400

    feat: combine add and edit table forms

--- client/src/features/tables/TableList.js
+++ client/src/features/tables/TableList.js
@@ -1,102 +1,195 @@
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as yup from "yup";
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
-import { updateTable, deleteTable } from "./tablesSlice";
+import { updateTable, addNewTable, deleteTable } from "./tablesSlice";
 
-function TableList({
-  tables,
-  onTableClick,
-  selectedSpot,
-  onDelete,
-  tableSchema,
-}) {
+function TableList({ tables, onTableClick, selectedSpot }) {
   const dispatch = useDispatch();
-
   const [editFormHidden, setEditFormHidden] = useState(true);
   const [tableToEdit, setTableToEdit] = useState(null);
   const sortedTables = [...tables].sort((a, b) => a.number - b.number);
 
+  const tableSchema = yup.object().shape({
+    number: yup
+      .number()
+      .required("Number is required")
+      .test("unique-number", "Table number already exists", function (value) {
+        return !tables.some((table) => table.number === value);
+      }),
+    capacity: yup
+      .number()
+      .min(1, "Capacity must be at least 1")
+      .required("Capacity is required"),
+  });
+
   return (
-    <div>
+    <div className="p-6 bg-gray-100 rounded-md shadow-lg">
+      {/* Add New Table Button */}
+      {editFormHidden && (
+        <button
+          className="bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded mb-4"
+          onClick={() => {
+            setEditFormHidden(false);
+            setTableToEdit(null);
+          }}
+        >
+          Add New Table
+        </button>
+      )}
+
+      {/* Form Section for Add/Edit */}
       {!editFormHidden && (
-        <div>
+        <div className="bg-white p-6 rounded-md shadow-md mb-6">
           <Formik
             initialValues={{
-              number: "",
-              capacity: "",
+              number: tableToEdit?.number || "",
+              capacity: tableToEdit?.capacity || "",
             }}
             validationSchema={tableSchema}
+            enableReinitialize={true}
             onSubmit={(values, { setSubmitting, resetForm }) => {
-              dispatch(updateTable({ ...values, id: tableToEdit.id }))
-                .unwrap()
-                .then((res) => {
-                  console.log("Table updated successfully:", res);
-                  resetForm();
-                })
-                .catch((err) => {
-                  console.error("Error updating table:", err);
-                })
-                .finally(() => {
-                  setSubmitting(false);
-                  setEditFormHidden(true);
-                });
+              if (tableToEdit) {
+                dispatch(updateTable({ ...values, id: tableToEdit.id }))
+                  .unwrap()
+                  .then((res) => {
+                    console.log("Table updated successfully:", res);
+                    resetForm();
+                  })
+                  .catch((err) => {
+                    console.error("Error updating table:", err);
+                  })
+                  .finally(() => {
+                    setSubmitting(false);
+                    setEditFormHidden(true);
+                    setTableToEdit(null);
+                  });
+              } else {
+                dispatch(addNewTable(values))
+                  .unwrap()
+                  .then((res) => {
+                    console.log("Table added successfully:", res);
+                    resetForm();
+                  })
+                  .catch((err) => {
+                    console.error("Error adding table:", err);
+                  })
+                  .finally(() => {
+                    setSubmitting(false);
+                    setEditFormHidden(true);
+                  });
+              }
             }}
           >
             {({ isSubmitting }) => (
               <Form>
-                <label htmlFor="number">Number</label>
-                <Field name="number" type="number" />
-                <ErrorMessage name="number" component="div" />
-                <label htmlFor="capacity">Capacity</label>
-                <Field name="capacity" type="number" />
-                <ErrorMessage name="capacity" component="div" />
-                <button
-                  type="submit"
-                  disabled={isSubmitting}
-                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                >
-                  Save
-                </button>
+                {/* Form Fields */}
+                <div className="mb-4">
+                  <label
+                    htmlFor="number"
+                    className="block text-sm font-medium text-gray-700 mb-1"
+                  >
+                    Table Number
+                  </label>
+                  <Field
+                    name="number"
+                    type="number"
+                    className="border border-gray-300 rounded-md w-full p-2"
+                  />
+                  <ErrorMessage
+                    name="number"
+                    component="div"
+                    className="text-red-600 text-sm mt-1"
+                  />
+                </div>
+
+                <div className="mb-4">
+                  <label
+                    htmlFor="capacity"
+                    className="block text-sm font-medium text-gray-700 mb-1"
+                  >
+                    Capacity
+                  </label>
+                  <Field
+                    name="capacity"
+                    type="number"
+                    className="border border-gray-300 rounded-md w-full p-2"
+                  />
+                  <ErrorMessage
+                    name="capacity"
+                    component="div"
+                    className="text-red-600 text-sm mt-1"
+                  />
+                </div>
+
+                {/* Submit Button */}
+                <div className="flex space-x-4">
+                  <button
+                    type="submit"
+                    disabled={isSubmitting}
+                    className={`${
+                      tableToEdit ? "bg-green-600" : "bg-blue-600"
+                    } hover:bg-opacity-90 text-white font-bold py-2 px-4 rounded`}
+                  >
+                    {tableToEdit ? "Update Table" : "Add Table"}
+                  </button>
+
+                  {/* Cancel Button */}
+                  <button
+                    type="button"
+                    onClick={() => {
+                      setEditFormHidden(true);
+                      setTableToEdit(null);
+                    }}
+                    className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+                  >
+                    Cancel
+                  </button>
+                </div>
               </Form>
             )}
           </Formik>
         </div>
       )}
-      <ul className="list-none space-y-2">
+
+      {/* Table List */}
+      <ul className="list-none space-y-4">
         {sortedTables.map((table) => (
-          <li key={table.id} className="flex justify-between items-center">
+          <li
+            key={table.id}
+            className="bg-white p-4 rounded-md shadow-md flex justify-between items-center"
+          >
+            {/* Select Table Button */}
             {selectedSpot && (
               <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                className="bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded"
                 onClick={() => onTableClick(table.id)}
               >
                 Select
               </button>
             )}
-            <span>
+
+            {/* Table Information */}
+            <span className="text-lg font-semibold">
               Table: {table.number} | Capacity: {table.capacity}
             </span>
+
+            {/* Edit and Delete Buttons */}
             <div className="flex gap-2">
               <button
-                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                className="bg-green-600 hover:bg-green-800 text-white font-bold py-2 px-4 rounded"
                 onClick={() => {
-                  if (tableToEdit === table) {
-                    setEditFormHidden(true);
-                  } else {
-                    setEditFormHidden(false);
-                    setTableToEdit(table);
-                  }
+                  setEditFormHidden(false);
+                  setTableToEdit(table);
                 }}
               >
                 Edit
               </button>
               <button
-                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                className="bg-red-600 hover:bg-red-800 text-white font-bold py-2 px-4 rounded"
                 onClick={() => {
                   dispatch(
-                    deleteTable({
-                      tableId: table.id,
-                    })
+                    deleteTable({ tableId: table.id })
                       .unwrap()
                       .then((res) => {
                         console.log("Table deleted successfully:", res);
@@ -112,10 +205,12 @@ function TableList({
             </div>
           </li>
         ))}
+
+        {/* Remove Assignment Button */}
         {selectedSpot && selectedSpot.tableId && (
           <button
-            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-            onClick={() => onTableClick(null)} // Clear assignment
+            className="bg-red-600 hover:bg-red-800 text-white font-bold py-2 px-4 rounded mt-4"
+            onClick={() => onTableClick(null)}
           >
             Remove
           </button>

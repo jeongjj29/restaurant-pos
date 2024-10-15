ec7db949a5bd869e83c33e716685bad4e9f3fb11
Jae Jeong <jeongjj29@gmail.com>
2024-10-15 01:20:28 -0400

    feat: add menuItems and menuCategories to store

--- client/src/store.js
+++ client/src/store.js
@@ -3,11 +3,11 @@ import employeesReducer from "./features/employees/employeesSlice";
 import tablesReducer from "./features/tables/tablesSlice";
 // import ordersReducer from "./features/orders/ordersSlice";
 // import orderItemsReducer from "./features/orderItems/orderItemsSlice";
-// import menuItemsReducer from "./features/menuItems/menuItemsSlice";
+import menuItemsReducer from "./features/menu/menuItemsSlice";
 // import paymentsReducer from "./features/payments/paymentsSlice";
 // import discountsReducer from "./features/discounts/discountsSlice";
 // import rolesReducer from "./features/roles/rolesSlice";
-// import menuCategoriesReducer from "./features/menuCategories/menuCategoriesSlice";
+import menuCategoriesReducer from "./features/menu/menuCategoriesSlice";
 
 export const store = configureStore({
   reducer: {
@@ -15,10 +15,10 @@ export const store = configureStore({
     tables: tablesReducer,
     // orders: ordersReducer,
     // orderItems: orderItemsReducer,
-    // menuItems: menuItemsReducer,
+    menuItems: menuItemsReducer,
     // payments: paymentsReducer,
     // discounts: discountsReducer,
     // roles: rolesReducer,
-    // menuCategories: menuCategoriesReducer,
+    menuCategories: menuCategoriesReducer,
   },
 });

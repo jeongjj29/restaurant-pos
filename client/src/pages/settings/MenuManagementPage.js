b43a3158c9ad6e91db97f09aba531b87ad591825
Jae Jeong <jeongjj29@gmail.com>
2024-10-15 01:16:04 -0400

    feat: add components to menu management page

--- client/src/pages/settings/MenuManagementPage.js
+++ client/src/pages/settings/MenuManagementPage.js
@@ -1,7 +1,15 @@
 import React from "react";
+import MenuLayout from "../../features/menu/MenuLayout";
+import MenuItemList from "../../features/menu/MenuItemList";
 
 function MenuManagementPage() {
-  return <div>MenuManagementPage</div>;
+  return (
+    <div>
+      MenuManagementPage
+      <MenuLayout />
+      <MenuItemList />
+    </div>
+  );
 }
 
 export default MenuManagementPage;

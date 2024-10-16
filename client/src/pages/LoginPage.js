230a7ed939fcd516fc02b7da0a601cc884d27c55
Jae Jeong <jeongjj29@gmail.com>
2024-10-16 18:30:49 -0400

    feat: add login form to LoginPage

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,7 +1,41 @@
 import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as yup from "yup";
+import { login } from "../features/auth/authService";
 
 function LoginPage() {
-  return <div>LoginPage</div>;
+  const loginSchema = yup.object().shape({
+    username: yup.string().required("Username is required"),
+    password: yup.string().required("Password is required"),
+  });
+
+  return (
+    <div>
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        validationSchema={loginSchema}
+        onSubmit={(values, { setSubmitting, resetForm }) => {
+          login(values.username, values.password);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <label htmlFor="username">Username:</label>
+            <Field type="text" name="username" placeholder="Username" />
+            <ErrorMessage name="username" component="div" />
+
+            <label htmlFor="password">Password:</label>
+            <Field type="password" name="password" placeholder="Password" />
+            <ErrorMessage name="password" component="div" />
+
+            <button type="submit" disabled={isSubmitting}>
+              Login
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 }
 
 export default LoginPage;

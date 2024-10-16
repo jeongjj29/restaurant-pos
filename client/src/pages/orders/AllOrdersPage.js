0bde42d716b47b736526f52860a6250ec5be3e23
Jae Jeong <jeongjj29@gmail.com>
2024-10-16 13:50:04 -0400

    feat: create settle button that redirects to payment page

--- client/src/pages/orders/AllOrdersPage.js
+++ client/src/pages/orders/AllOrdersPage.js
@@ -1,14 +1,31 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import AllOrdersTable from "../../features/orders/AllOrdersTable";
 import OrderDetails from "../../features/orders/OrderDetails";
 
 function AllOrdersPage() {
+  const navigate = useNavigate();
   const [clickedOrder, setClickedOrder] = React.useState(null);
 
   return (
     <div className="flex flex-row">
       <AllOrdersTable setClickedOrder={setClickedOrder} />
-      <OrderDetails clickedOrder={clickedOrder} />
+      <OrderDetails order={clickedOrder} />
+      {clickedOrder && (
+        <div className="flex flex-col justify-center items-center">
+          <button className="bg-green-300 hover:bg-green-400 text-white font-bold py-2 px-4 m-4 rounded">
+            Print
+          </button>
+          <button
+            onClick={() => {
+              navigate("/orders/payment", { state: { clickedOrder } });
+            }}
+            className="bg-green-300 hover:bg-green-400 text-white font-bold py-2 px-4 m-4 rounded"
+          >
+            Settle
+          </button>
+        </div>
+      )}
     </div>
   );
 }

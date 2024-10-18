7570f72c14fbd2d872753efb0e4d8fcf94cb68fc
Jae Jeong <jeongjj29@gmail.com>
2024-10-18 18:00:08 -0400

    refactor: remove selectedSpot state

--- client/src/features/tables/TableList.js
+++ client/src/features/tables/TableList.js
@@ -4,7 +4,7 @@ import { deleteTable } from "./tablesSlice";
 import TableForm from "./TableForm";
 import DraggableTable from "./DraggableTable";
 
-function TableList({ tables, onTableClick, selectedSpot, setSelectedSpot }) {
+function TableList({ tables }) {
   const dispatch = useDispatch();
   const [editFormHidden, setEditFormHidden] = useState(true);
   const [tableToEdit, setTableToEdit] = useState(null);
@@ -12,18 +12,8 @@ function TableList({ tables, onTableClick, selectedSpot, setSelectedSpot }) {
 
   return (
     <div className="w-160 h-screen overflow-y-auto bg-white p-4 shadow-lg p-6 bg-gray-100 rounded-md shadow-lg">
-      {/* Cancel Table Selection Button */}
-      {selectedSpot && (
-        <button
-          onClick={() => setSelectedSpot(null)}
-          className="bg-red-600 hover:bg-red-800 text-white font-bold py-2 px-4 rounded mb-4"
-        >
-          Cancel Table Selection
-        </button>
-      )}
-
       {/* Add New Table Button */}
-      {editFormHidden && !selectedSpot && (
+      {editFormHidden && (
         <button
           className="bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded mb-4"
           onClick={() => {
@@ -59,59 +49,36 @@ function TableList({ tables, onTableClick, selectedSpot, setSelectedSpot }) {
               capacity={table.capacity}
             />
 
-            {/* Select Table Button */}
-            {selectedSpot && (
-              <button
-                className="bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded"
-                onClick={() => onTableClick(table.id)}
-              >
-                Select
-              </button>
-            )}
-
             {/* Edit and Delete Buttons */}
             <div className="flex gap-2">
-              {!selectedSpot && (
-                <button
-                  className="bg-green-600 hover:bg-green-800 text-white font-bold py-2 px-4 rounded"
-                  onClick={() => {
-                    setEditFormHidden(false);
-                    setTableToEdit(table);
-                  }}
-                >
-                  Edit
-                </button>
-              )}
-              {!selectedSpot && (
-                <button
-                  className="bg-red-600 hover:bg-red-800 text-white font-bold py-2 px-4 rounded"
-                  onClick={() => {
-                    dispatch(deleteTable(table.id)) // Pass just the table ID
-                      .unwrap() // Unwrap the result to handle the actual promise
-                      .then((res) => {
-                        console.log("Table deleted successfully:", res);
-                      })
-                      .catch((err) => {
-                        console.error("Error deleting table:", err);
-                      });
-                  }}
-                >
-                  Delete
-                </button>
-              )}
+              <button
+                className="bg-green-600 hover:bg-green-800 text-white font-bold py-2 px-4 rounded"
+                onClick={() => {
+                  setEditFormHidden(false);
+                  setTableToEdit(table);
+                }}
+              >
+                Edit
+              </button>
+
+              <button
+                className="bg-red-600 hover:bg-red-800 text-white font-bold py-2 px-4 rounded"
+                onClick={() => {
+                  dispatch(deleteTable(table.id)) // Pass just the table ID
+                    .unwrap() // Unwrap the result to handle the actual promise
+                    .then((res) => {
+                      console.log("Table deleted successfully:", res);
+                    })
+                    .catch((err) => {
+                      console.error("Error deleting table:", err);
+                    });
+                }}
+              >
+                Delete
+              </button>
             </div>
           </li>
         ))}
-
-        {/* Remove Assignment Button */}
-        {selectedSpot && selectedSpot.tableId && (
-          <button
-            className="bg-red-600 hover:bg-red-800 text-white font-bold py-2 px-4 rounded mt-4"
-            onClick={() => onTableClick(null)}
-          >
-            Remove
-          </button>
-        )}
       </ul>
     </div>
   );

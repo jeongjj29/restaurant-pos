99e60b8f419c9d5c6d69322fa0276915bd5330ba
Jae Jeong <jeongjj29@gmail.com>
2024-10-20 19:39:36 -0400

    feat: loads OrderInput component and passes tableId

--- client/src/pages/orders/DineInOrderInputPage.js
+++ client/src/pages/orders/DineInOrderInputPage.js
@@ -1,7 +1,14 @@
 import React from "react";
+import OrderInput from "../../features/orders/OrderInput";
+import { useParams } from "react-router-dom";
 
 function DineInOrderInputPage() {
-  return <div>DineInOrderInputPage</div>;
+  const tableId = useParams().tableId;
+  return (
+    <div>
+      <OrderInput tableId={tableId} />
+    </div>
+  );
 }
 
 export default DineInOrderInputPage;

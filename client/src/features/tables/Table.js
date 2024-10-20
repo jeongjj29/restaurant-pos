e84eb5f3e081679cae590a07e47de509f375af92
Jae Jeong <jeongjj29@gmail.com>
2024-10-20 19:16:38 -0400

    feat: on table click, navigate to new order input page or orders by table page

--- client/src/features/tables/Table.js
+++ client/src/features/tables/Table.js
@@ -1,13 +1,21 @@
+import axios from "axios";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-function Table({ isTable, tableId, number, xIndex, yIndex }) {
+function Table({ isTable, tableId, number, orders }) {
   const navigate = useNavigate();
   if (isTable) {
     return (
       <div
         onClick={() => {
-          navigate(`/orders/table/${number}`);
+          axios.get(`/orders/table/${tableId}`).then((res) => {
+            const orders = res.data;
+            if (orders.length > 0) {
+              navigate(`/orders/${tableId}`);
+            } else {
+              navigate(`/orders/table/${tableId}/new`);
+            }
+          });
         }}
         className="text-2xl flex items-center justify-center w-24 h-24 border-2 rounded-lg text-white cursor-pointer bg-slate-400"
       >

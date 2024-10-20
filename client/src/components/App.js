d03a296a4981bdd08af07e5c720923e64f7ec8e2
Jae Jeong <jeongjj29@gmail.com>
2024-10-20 18:30:56 -0400

    feat: fetch data on app start

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
+import { useDispatch } from "react-redux";
+import { fetchTables } from "../features/tables/tablesSlice";
+import { fetchOrders } from "../features/orders/ordersSlice";
+import { fetchEmployees } from "../features/employees/employeesSlice";
+import { fetchMenuItems } from "../features/menu/menuItemsSlice";
+import { fetchMenuCategories } from "../features/menu/menuCategoriesSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchTables());
+    dispatch(fetchOrders());
+    dispatch(fetchEmployees());
+    dispatch(fetchMenuItems());
+    dispatch(fetchMenuCategories());
+  }, [dispatch]);
+
   return (
     <div className="h-screen w-screen flex flex-row bg-gray-100">
       {/* NavBar with fixed width and full height */}

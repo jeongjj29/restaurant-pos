bdc6340e47a365a87398a0486bc2b367f798eabe
Jae Jeong <jeongjj29@gmail.com>
2024-10-21 16:32:37 -0400

    feat: convert all icons into buttons and add paymentType state

--- client/src/pages/orders/PaymentsPage.js
+++ client/src/pages/orders/PaymentsPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocation } from "react-router-dom";
 import OrderDetails from "../../features/orders/OrderDetails";
 import { PaymentIcon } from "react-svg-credit-card-payment-icons";
@@ -7,7 +7,7 @@ function PaymentsPage() {
   const location = useLocation();
   const { order } = location.state;
 
-  console.log(order);
+  const [paymentType, setPaymentType] = useState("");
 
   return (
     <div className="flex flex-row">
@@ -16,14 +16,23 @@ function PaymentsPage() {
       </div>
       <div className="flex flex-row">
         <div className="flex flex-col gap-4">
-          <button></button>
-          <PaymentIcon type="visa" format="flatRounded" width={100} />
-          <PaymentIcon type="mastercard" format="flatRounded" width={100} />
-          <PaymentIcon type="amex" format="flatRounded" width={100} />
-          <PaymentIcon type="discover" format="flatRounded" width={100} />
-          <div className="flex items-center justify-center bg-zinc-200 w-iconw py-3 h-18 border-2 rounded-lg p-0">
-            <span className="text-3xl font-semibold">CASH</span>
-          </div>
+          <button>
+            <PaymentIcon type="visa" format="flatRounded" width={100} />
+          </button>
+          <button>
+            <PaymentIcon type="mastercard" format="flatRounded" width={100} />
+          </button>
+          <button>
+            <PaymentIcon type="amex" format="flatRounded" width={100} />
+          </button>
+          <button>
+            <PaymentIcon type="discover" format="flatRounded" width={100} />
+          </button>
+          <button>
+            <div className="flex items-center justify-center bg-zinc-200 w-iconw py-3 h-18 border-2 rounded-lg p-0">
+              <span className="text-3xl font-semibold">CASH</span>
+            </div>
+          </button>
         </div>
         <div>
           <h1>Payment Amount:</h1>

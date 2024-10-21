7ffe7894cbde5645732e106ee07d6c29e520bf00
Jae Jeong <jeongjj29@gmail.com>
2024-10-21 16:40:24 -0400

    feat: separate card type into different types of cards

--- server/models.py
+++ server/models.py
@@ -10,7 +10,7 @@ from config import db, bcrypt
 
 ORDER_STATUS = ["open", "closed"]
 ORDER_TYPE = ["dine_in", "take_out"]
-PAYMENT_TYPE = ["cash", "card"]
+PAYMENT_TYPE = ["cash", "visa", "mastercard", "amex", "discover"]
 ACCESS_LEVEL = [1, 2, 3, 4]
 
 

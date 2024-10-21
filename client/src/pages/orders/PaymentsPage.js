1193ab668f4671d486c6b16606636dfd573d37b6
Jae Jeong <jeongjj29@gmail.com>
2024-10-21 16:24:35 -0400

    feat: add cash payment icon

--- client/src/pages/orders/PaymentsPage.js
+++ client/src/pages/orders/PaymentsPage.js
@@ -10,15 +10,24 @@ function PaymentsPage() {
   console.log(order);
 
   return (
-    <div className="">
-      <div>
+    <div className="flex flex-row">
+      <div className="">
         <OrderDetails order={order} />
       </div>
-      <div>
-        <PaymentIcon type="visa" format="flatRounded" width={100} />
-        <PaymentIcon type="mastercard" format="flatRounded" width={100} />
-        <PaymentIcon type="amex" format="flatRounded" width={100} />
-        <PaymentIcon type="discover" format="flatRounded" width={100} />
+      <div className="flex flex-row">
+        <div className="flex flex-col gap-4">
+          <button></button>
+          <PaymentIcon type="visa" format="flatRounded" width={100} />
+          <PaymentIcon type="mastercard" format="flatRounded" width={100} />
+          <PaymentIcon type="amex" format="flatRounded" width={100} />
+          <PaymentIcon type="discover" format="flatRounded" width={100} />
+          <div className="flex items-center justify-center bg-zinc-200 w-iconw py-3 h-18 border-2 rounded-lg p-0">
+            <span className="text-3xl font-semibold">CASH</span>
+          </div>
+        </div>
+        <div>
+          <h1>Payment Amount:</h1>
+        </div>
       </div>
     </div>
   );

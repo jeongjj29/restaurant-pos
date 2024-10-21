8257ce06c6233e65a686de94c560795f3fae5e32
Jae Jeong <jeongjj29@gmail.com>
2024-10-21 16:24:00 -0400

    style: add custom width for icons

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        iconw: "100px",
+      },
+    },
   },
   plugins: [],
 };

09c9e20cc548074918836bf8839c3746fe7f9875
Jae Jeong <jeongjj29@gmail.com>
2024-10-21 23:29:25 -0400

    feat: lists user's orders

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import AllOrdersTable from "../features/orders/AllOrdersTable";
 
 function Home() {
-  return <div>Home</div>;
+  const orders = useSelector((state) => state.orders.orders);
+  const user = useSelector((state) => state.auth.user);
+  const userOrders = orders.filter((order) => order.user_id === user.id);
+
+  console.log(user);
+  console.log(userOrders);
+
+  return (
+    <div>
+      <h1>Hello {user.first_name}</h1>
+      <div>
+        <h2>Your Orders:</h2>
+        <AllOrdersTable orders={userOrders} />
+      </div>
+    </div>
+  );
 }
 
 export default Home;

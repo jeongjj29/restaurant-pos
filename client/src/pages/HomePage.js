f2eca603d767de2ce7ddd3176a9d285866a2d27b
Jae Jeong <jeongjj29@gmail.com>
2024-10-22 18:55:51 -0400

    feat: add order click handler on homepage AllOrdersTable

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import AllOrdersTable from "../features/orders/AllOrdersTable";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+  const navigate = useNavigate();
   const orders = useSelector((state) => state.orders.orders);
   const user = useSelector((state) => state.auth.user);
   const userOrders = orders.filter((order) => order.user_id === user.id);
@@ -10,12 +12,19 @@ function Home() {
   console.log(user);
   console.log(userOrders);
 
+  const handleOrderClick = (order) => {
+    navigate("/orders/payment", { state: { order } });
+  };
+
   return (
     <div>
       <h1>Hello {user.first_name}</h1>
       <div>
-        <h2>Your Orders:</h2>
-        <AllOrdersTable orders={userOrders} />
+        <h2 className="text-lg font-semibold ml-10 mt-10">Your Orders:</h2>
+        <AllOrdersTable
+          orders={userOrders}
+          setClickedOrder={handleOrderClick}
+        />
       </div>
     </div>
   );

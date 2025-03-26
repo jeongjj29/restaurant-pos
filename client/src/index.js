d0ea97864f7808a37de2744a7ab387265e7749a6
Jae Jeong <jeongjj29@gmail.com>
2025-03-26 17:47:57 -0400

    refactor: simplify root rendering by inlining createRoot call

--- client/src/index.js
+++ client/src/index.js
@@ -8,9 +8,7 @@ import routes from "./routes";
 
 const router = createBrowserRouter(routes);
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <RouterProvider router={router} />

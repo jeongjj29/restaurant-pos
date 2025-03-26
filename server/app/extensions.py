aac39f75c62e37e351bee0d71503a79302691818
Jae Jeong <jeongjj29@gmail.com>
2025-03-26 19:24:51 -0400

    refactor: implement app factory pattern and modularize config and extensions

---
+++ server/app/extensions.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_restful import Api
+from flask_bcrypt import Bcrypt
+from flask_cors import CORS
+from sqlalchemy import MetaData
+
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
+
+db = SQLAlchemy(metadata=metadata)
+migrate = Migrate()
+api = Api(prefix="/api")
+bcrypt = Bcrypt()
+cors = CORS

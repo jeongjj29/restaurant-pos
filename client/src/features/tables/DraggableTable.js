1f8c4d4c0171293620f984aee3c59ee79cab3ea6
Jae Jeong <jeongjj29@gmail.com>
2025-03-26 07:27:36 -0400

    style: remove transform

--- client/src/features/tables/DraggableTable.js
+++ client/src/features/tables/DraggableTable.js
@@ -5,24 +5,12 @@ function DraggableTable({ tableId, number, capacity }) {
     id: tableId,
   });
 
-  const style = {
-    transform: transform
-      ? `translate3d(${transform.x}px, ${transform.y}px, 0)`
-      : undefined,
-
-    zIndex: transform ? 50 : "auto",
-    cursor: transform ? "grabbing" : "grab",
-  };
-
   return (
     <div
       ref={setNodeRef}
-      style={style}
       {...attributes}
       {...listeners}
-      className={`text-lg font-semibold flex flex-col justify-center items-center rounded-md mr-2 px-2 py-4 transition duration-200 ${
-        transform ? "bg-white/10 shadow-lg scale-105" : "hover:bg-white/10"
-      }`}
+      className="text-lg font-semibold flex flex-col justify-center items-center rounded-md mr-2 px-2 py-4 transition duration-200 hover:bg-white/10"
     >
       <p>Table: {number}</p>
       <p>Seats: {capacity}</p>

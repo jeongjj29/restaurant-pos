a8fcc5fc93ffcd1dc20acfc7750bfe741c1d15ea
Jae Jeong <jeongjj29@gmail.com>
2025-03-27 03:32:26 -0400

    feat: add access level 5

--- server/app/constants.py
+++ server/app/constants.py
@@ -1,4 +1,4 @@
 ORDER_STATUS = ["open", "closed"]
 ORDER_TYPE = ["dine_in", "take_out"]
 PAYMENT_TYPE = ["cash", "visa", "mastercard", "amex", "discover"]
-ACCESS_LEVEL = [1, 2, 3, 4]
+ACCESS_LEVEL = [1, 2, 3, 4, 5]

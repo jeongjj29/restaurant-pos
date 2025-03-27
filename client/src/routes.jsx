14bf09878e9de2ac2afa6daa787dfc5c66c14477
Jae Jeong <jeongjj29@gmail.com>
2025-03-27 17:36:55 -0400

    refactor: change path: "" to index: true

--- client/src/routes.jsx
+++ client/src/routes.jsx
@@ -23,7 +23,7 @@ const routes = [
     errorElement: <ErrorPage />,
     children: [
       {
-        path: "",
+        index: true,
         element: (
           <ProtectedRoute>
             <Home />

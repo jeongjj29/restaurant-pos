7ea8b1381a13c0a2d3c7b96fb31b397a8020670a
Jae Jeong <jeongjj29@gmail.com>
2025-03-27 07:53:41 -0400

    fix: initialize jwt with app

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from config import Config
-from .extensions import db, migrate, api, bcrypt, cors
+from .extensions import db, migrate, api, bcrypt, cors, jwt
 from .routes import register_blueprints
 
 def create_app():
@@ -12,6 +12,8 @@ def create_app():
     api.init_app(app)
     bcrypt.init_app(app)
     cors.init_app(app)
+    jwt.init_app(app)
+
 
     register_blueprints(app)
 

600f6a3e5b425f9e38c6c75e6bb3c4db56e24cf9
Jae Jeong <jeongjj29@gmail.com>
2025-03-28 03:06:55 -0400

    refactor: update store import path

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "@store";
-import "./index.css";
+import { store } from "@app/store";
 import routes from "@routes";
+import "./index.css";
 
 const router = createBrowserRouter(routes, {
   future: {

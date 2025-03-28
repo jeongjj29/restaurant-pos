e11792d2b4e2f83a298b8e77db7e25a3739a8d10
Jae Jeong <jeongjj29@gmail.com>
2025-03-28 02:39:30 -0400

    feat: define RootState

--- client/src/store.ts
+++ client/src/store.ts
@@ -1,26 +1,29 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authSliceReducer from "@auth/authSlice";
+// import discountsReducer from "@features/discounts/slices/discountsSlice";
 import employeesReducer from "@employees/slices/employeesSlice";
-import tablesReducer from "@tables/slices/tablesSlice";
-import ordersReducer from "@orders/slices/ordersSlice";
 import orderItemsReducer from "@orders/slices/orderItemsSlice";
+import ordersReducer from "@orders/slices/ordersSlice";
+import menuCategoriesReducer from "@menu/slices/menuCategoriesSlice";
 import menuItemsReducer from "@menu/slices/menuItemsSlice";
 import paymentsReducer from "@payments/slices/paymentsSlice";
-// import discountsReducer from "@features/discounts/slices/discountsSlice";
 import rolesReducer from "@employees/slices/rolesSlice";
-import menuCategoriesReducer from "@menu/slices/menuCategoriesSlice";
-import authSliceReducer from "@auth/authSlice";
+import tablesReducer from "@tables/slices/tablesSlice";
 
 export const store = configureStore({
   reducer: {
+    // discounts: discountsReducer,
+    auth: authSliceReducer,
     employees: employeesReducer,
-    tables: tablesReducer,
     orders: ordersReducer,
     orderItems: orderItemsReducer,
+    menuCategories: menuCategoriesReducer,
     menuItems: menuItemsReducer,
     payments: paymentsReducer,
-    // discounts: discountsReducer,
     roles: rolesReducer,
-    menuCategories: menuCategoriesReducer,
-    auth: authSliceReducer,
+    tables: tablesReducer,
   },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;

1bdd79bc34919d8af6dabfe978d5e7861497c5e8
Jae Jeong <jeongjj29@gmail.com>
2025-03-28 03:01:49 -0400

    refactor: add alias to src/app/* and remove @store

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,29 +1,25 @@
-import react from "@vitets/plugin-react";
 import { defineConfig } from "vite";
-import { fileURLToPath } from "url";
-import { dirname, resolve } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@assets": resolve(__dirname, "src/assets"),
-      "@components": resolve(__dirname, "src/components"),
-      "@features": resolve(__dirname, "src/features"),
-      "@pages": resolve(__dirname, "src/pages"),
-      "@constants": resolve(__dirname, "src/constants.ts"),
-      "@routes": resolve(__dirname, "src/routes.ts"),
-      "@store": resolve(__dirname, "src/store.ts"),
-      "@utils": resolve(__dirname, "src/utils.ts"),
-      "@auth": resolve(__dirname, "src/features/auth"),
-      "@employees": resolve(__dirname, "src/features/employees"),
-      "@menu": resolve(__dirname, "src/features/menu"),
-      "@orders": resolve(__dirname, "src/features/orders"),
-      "@payments": resolve(__dirname, "src/features/payments"),
-      "@tables": resolve(__dirname, "src/features/tables"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@features": path.resolve(__dirname, "src/features"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@constants": path.resolve(__dirname, "src/constants.ts"),
+      "@routes": path.resolve(__dirname, "src/routes.ts"),
+      "@app": path.resolve(__dirname, "src/app"),
+      "@utils": path.resolve(__dirname, "src/utils.ts"),
+      "@auth": path.resolve(__dirname, "src/features/auth"),
+      "@employees": path.resolve(__dirname, "src/features/employees"),
+      "@menu": path.resolve(__dirname, "src/features/menu"),
+      "@orders": path.resolve(__dirname, "src/features/orders"),
+      "@payments": path.resolve(__dirname, "src/features/payments"),
+      "@tables": path.resolve(__dirname, "src/features/tables"),
     },
   },
   server: {

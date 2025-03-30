ac931d31263695ed358a92ab93bad6af09946ade
Jae Jeong <jeongjj29@gmail.com>
2025-03-30 04:00:00 -0400

    chore: replace TAX object constant with TAX_RATE number

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,6 +1,4 @@
-export const TAX = {
-  RATE: 0.08875,
-};
+export const TAX_RATE = 0.08875;
 
 export const TABLE_LAYOUT = {
   WIDTH: 5 as const,
--- client/src/utils/pricingUtils.ts
+++ client/src/utils/pricingUtils.ts
@@ -1,11 +1,11 @@
-import { TAX } from "@constants";
+import { TAX_RATE } from "@constants";
 import { OrderItem } from "@orders/types";
 
 export const subtotalPrice = (items: OrderItem[]): number =>
   items.reduce((acc, item) => acc + item.price * item.quantity, 0);
 
 export const taxPrice = (items: OrderItem[]): number =>
-  subtotalPrice(items) * TAX.RATE;
+  subtotalPrice(items) * TAX_RATE;
 
 export const totalPriceWithTax = (items: OrderItem[]): number =>
   parseFloat((subtotalPrice(items) + taxPrice(items)).toFixed(2));

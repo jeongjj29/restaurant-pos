a9bb9084bece482e5eade5c14e3f80b8c151ce21
Jae Jeong <jeongjj29@gmail.com>
2025-04-01 02:22:29 -0400

    feat: update TableList to TypeScript

--- client/src/features/tables/components/TableList.tsx
+++ client/src/features/tables/components/TableList.tsx
@@ -1,20 +1,25 @@
 import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useAppDispatch } from "@app/hooks";
 import { deleteTable } from "../slices/tablesSlice";
 import TableForm from "./TableForm";
 import DraggableTable from "./DraggableTable";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
+import { Table } from "@tables/types";
 
-function TableList({ tables }) {
-  const dispatch = useDispatch();
+interface TableListProps {
+  tables: Table[];
+}
+
+function TableList({ tables }: TableListProps) {
+  const dispatch = useAppDispatch();
   const [editFormHidden, setEditFormHidden] = useState(true);
-  const [tableToEdit, setTableToEdit] = useState(null);
+  const [tableToEdit, setTableToEdit] = useState<Table | null>(null);
+
   const sortedTables = [...tables].sort((a, b) => a.number - b.number);
 
   return (
     <div className="h-full p-4 px-8 rounded-md shadow-lg bg-white/5">
-      {/* Add New Table Button */}
       {editFormHidden && (
         <button
           className="bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded-sm mb-4"
@@ -27,7 +32,6 @@ function TableList({ tables }) {
         </button>
       )}
 
-      {/* Form Section for Add/Edit */}
       {!editFormHidden && (
         <div className="flex h-full items-center justify-center">
           <TableForm
@@ -39,7 +43,6 @@ function TableList({ tables }) {
         </div>
       )}
 
-      {/* Table List */}
       {editFormHidden && (
         <div className="flex flex-col flex-wrap gap-4 max-h-full">
           {sortedTables.map((table) => (
@@ -47,14 +50,12 @@ function TableList({ tables }) {
               key={table.id}
               className="bg-white/5 h-fit p-2 rounded-md shadow-md flex items-center"
             >
-              {/* Table Information */}
               <DraggableTable
                 tableId={table.id}
                 number={table.number}
                 capacity={table.capacity}
               />
 
-              {/* Edit and Delete Buttons */}
               <div className="flex flex-col gap-2">
                 <button
                   className="bg-green-800 hover:bg-green-800 text-white font-bold py-2 px-2 rounded-sm"
@@ -69,8 +70,8 @@ function TableList({ tables }) {
                 <button
                   className="bg-red-800 hover:bg-red-800 text-white font-bold py-2 px-2 rounded-sm"
                   onClick={() => {
-                    dispatch(deleteTable(table.id)) // Pass just the table ID
-                      .unwrap() // Unwrap the result to handle the actual promise
+                    dispatch(deleteTable(table.id))
+                      .unwrap()
                       .then((res) => {
                         console.log("Table deleted successfully:", res);
                       })

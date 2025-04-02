1434625347f23375a1d2c768e0b1fb08b291dcc4
Jae Jeong <jeongjj29@gmail.com>
2025-04-02 05:18:52 -0400

    feat: add submitting state and refactor handleOrderSubmit

--- client/src/features/orders/components/OrderInput.tsx
+++ client/src/features/orders/components/OrderInput.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { addOrder, fetchOrders } from "../slices/ordersSlice";
 import { addOrderItem } from "../slices/orderItemsSlice";
@@ -20,18 +20,25 @@ function OrderInput({ tableId }: OrderInputProps) {
   const menuCategories = useAppSelector(
     (state: RootState) => state.menuCategories.menuCategories
   );
+  const tables = useAppSelector((state: RootState) => state.tables.tables);
+  const table = tables.find((table) => table.id === tableId);
+  const [submitting, setSubmitting] = useState<boolean>(false);
   const [selectedCategory, setSelectedCategory] = useState<MenuCategory>(
     menuCategories[0]
   );
   const [pendingOrderItems, setPendingOrderItems] = useState<
     (MenuItem & { quantity: number })[]
   >([]);
-  const tables = useAppSelector((state: RootState) => state.tables.tables);
-  const table = tables.find((table) => table.id === tableId);
 
   const subtotal = calculateSubtotal(pendingOrderItems);
   const tax = subtotal * TAX_RATE;
   const total = subtotal + tax;
+
+  useEffect(() => {
+    if (menuCategories.length > 0 && !selectedCategory) {
+      setSelectedCategory(menuCategories[0]);
+    }
+  }, [menuCategories]);
 
   const handleMenuItemClick = (menuItem: MenuItem) => {
     setPendingOrderItems((prevOrderItems) => {
@@ -51,74 +58,71 @@ function OrderInput({ tableId }: OrderInputProps) {
     });
   };
 
-  const handleOrderSubmit = () => {
-    dispatch(
-      addOrder({
-        type: table ? "dine_in" : "take_out",
-        total_price: total,
-        status: "open",
-        sales_tax: TAX_RATE,
-        user_id: 1,
-        table_id: table?.id || null,
-      })
-    )
-      .unwrap()
-      .then((res) => {
-        pendingOrderItems.forEach((item) => {
+  const handleOrderSubmit = async () => {
+    try {
+      setSubmitting(true);
+      const order = await dispatch(
+        addOrder({
+          type: table ? "dine_in" : "take_out",
+          total_price: total,
+          status: "open",
+          sales_tax: TAX_RATE,
+          user_id: 1,
+          table_id: table?.id || null,
+        })
+      ).unwrap();
+      await Promise.all(
+        pendingOrderItems.map((item) =>
           dispatch(
             addOrderItem({
-              order_id: res.id,
+              order_id: order.id,
               menu_item_id: item.id,
               price: item.price,
               quantity: item.quantity,
             })
-          ).catch(console.error);
-        });
-
-        dispatch(fetchOrders());
-        setPendingOrderItems([]);
-        navigate("/");
-      })
-      .catch(console.error);
+          )
+        )
+      );
+    } catch (error) {
+      console.error("Error submitting order:", error);
+    } finally {
+      setSubmitting(false);
+      dispatch(fetchOrders());
+      navigate("/orders");
+    }
   };
 
   return (
-    <div className="flex flex-col h-full w-full">
-      <div className="flex flex-row h-full w-full">
-        <div className="w-fit h-full flex flex-col justify-center m-6">
-          {menuCategories.map((menuCategory) => (
-            <button
-              key={menuCategory.id}
-              onClick={() => setSelectedCategory(menuCategory)}
-              className="w-full bg-white/10 hover:bg-green-400 text-2xl font-bold py-2 px-2 rounded-sm mb-2"
-            >
-              {menuCategory.name}
-            </button>
-          ))}
-        </div>
-
-        <MenuItemGrid
-          items={selectedCategory?.menu_items || []}
-          onClick={handleMenuItemClick}
-        />
-
-        <OrderSummaryTable
-          items={pendingOrderItems}
-          table={table}
-          subtotal={subtotal}
-          tax={tax}
-          total={total}
-        />
+    <div className="flex flex-row h-full w-full bg-white/5 rounded-md">
+      <div className="w-1/8 overflow-y-auto flex flex-col justify-start m-4">
+        {menuCategories.map((menuCategory) => (
+          <button
+            key={menuCategory.id}
+            onClick={() => setSelectedCategory(menuCategory)}
+            className={`w-full hover:bg-green-400 text-xl font-bold py-2 px-2 rounded-sm mb-2 ${
+              selectedCategory?.id === menuCategory.id
+                ? "bg-white/15"
+                : "bg-white/5"
+            }`}
+          >
+            {menuCategory.name}
+          </button>
+        ))}
       </div>
 
-      <div className="flex justify-center items-center w-full mt-4 p-4 bg-gray-100">
-        <button
-          onClick={handleOrderSubmit}
-          className="bg-green-300 hover:bg-green-400 text-white font-bold py-2 px-4 rounded-sm"
-        >
-          Submit
-        </button>
-      </div>
+      <MenuItemGrid
+        items={selectedCategory?.menu_items || []}
+        onClick={handleMenuItemClick}
+      />
+      <OrderSummaryTable
+        items={pendingOrderItems}
+        table={table}
+        subtotal={subtotal}
+        tax={tax}
+        total={total}
+        onClick={handleOrderSubmit}
+        submitting={submitting}
+      />
     </div>
   );
 }
